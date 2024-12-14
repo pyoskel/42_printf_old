3c1f3f70944bb227e92cd91f927fc4d3cf63dc62
pyoskel <pasigermany@gmail.com>
2024-12-14 10:27:30 +0100

    create printf.h

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 01:55:25 by pbartoch          #+#    #+#             */
+/*   Updated: 2024/12/13 05:00:59 by pbartoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h> //for variadic functions
+# include <unistd.h> //for write function
+
+int	ft_printf(const char *format, ...);
+
+#endif
