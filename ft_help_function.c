29f5115fd5fcdeb01a458e4bf02dd1d59551ea7d
pyoskel <pasigermany@gmail.com>
2025-01-24 23:19:26 +0100

    craete helper function

--- ft_help_function.c
+++ ft_help_function.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_char.c                                       :+:      :+:    :+:   */
+/*   ft_help_function.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:40:27 by pbartoch          #+#    #+#             */
-/*   Updated: 2025/01/21 15:40:57 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/01/24 21:08:40 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putchar_fd(char c, int fd)
+int	ft_putchar(char c)
 {
-	write(fd, &c, 1);
+	int	count;
+
+	count = 0;
+	write(1, &c, 1);
+	count++;
+	return (count);
+}
+
+int	main(void)
+{
+	ft_putchar(53);
+	return (0);
 }
