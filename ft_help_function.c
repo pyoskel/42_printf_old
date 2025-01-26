e89c04dfa3feb8fccc32bc34ed355c001a60c275
pyoskel <pasigermany@gmail.com>
2025-01-26 21:54:57 +0100

    maybe I should delete

--- ft_help_function.c
+++ ft_help_function.c
@@ -6,24 +6,24 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:40:27 by pbartoch          #+#    #+#             */
-/*   Updated: 2025/01/24 21:08:40 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/01/26 21:44:13 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putchar(char c)
-{
-	int	count;
+// int	ft_putchar(char c)
+// {
+// 	int	count;
 
-	count = 0;
-	write(1, &c, 1);
-	count++;
-	return (count);
-}
+// 	count = 0;
+// 	write(1, &c, 1);
+// 	count++;
+// 	return (count);
+// }
 
-int	main(void)
-{
-	ft_putchar(53);
-	return (0);
-}
+// int	main(void)
+// {
+// 	ft_putchar(53);
+// 	return (0);
+// }
