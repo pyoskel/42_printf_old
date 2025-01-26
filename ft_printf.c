3e31c64f9de47742d31c9a56de546946be4fed28
pyoskel <pasigermany@gmail.com>
2025-01-26 22:59:37 +0100

    test makefile

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,15 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 01:24:28 by pbartoch          #+#    #+#             */
-/*   Updated: 2025/01/26 21:51:54 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/01/26 22:50:45 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "ft_printf.h"
-#include <stdarg.h> //for variadic functions
-#include <string.h> //for variadic functions
-#include <unistd.h> //for write function
-#include "../libft/libft.h"
+#include "ft_printf.h"
+// #include <stdarg.h> //for variadic functions
+// #include <string.h> //for variadic functions
+// #include <unistd.h> //for write function
+// #include "../libft/libft.h"
 
 // int	putnbr_base(int num, char *base)
 // {
@@ -42,51 +42,52 @@
 // 	format++;
 // }
 
-int	handle_specifier(char specifier, va_list args)
-{
-	int	count;
-
-	if (specifier == 'c')
-		count = ft_putchar_fd(1, va_arg(args, int));
-	// else if (specifier == 's')
-	// 	count = ft_putstr_fd
-	// else if (specifier == 'd')
-	// 	count = ft_putnbr_fd
-	// else if (specifier == 'i')
-	// 	count = ft_putnbr_fd
-	// else if (specifier == 'u')
-	// 	count = ft_putnbr_fd
-	// else if (specifier == '%')
-	// 	count = ft_putchar_fd
-	// else if (specifier == 'x')
-	// 	count = ft_puthex
-	// else if (specifier == 'X')
-	// 	count = ft_puthex_upper
-	// else if (specifier == 'p')
-	// 	count = ft_putchar_fd or putptr
-	else
-		return (-1);
-	return (count);
-}
-
-int	ft_printf(const char *format, ...)
-{
-	int		i;
-	va_list	args;
-
-	va_start(args, format);
-	i = 0;
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-			replace_percent(++format, args);
-		else
-			write(1, &format, 1);
-		format++;
-	}
-}
-
-// int	main(void)
+// int	handle_specifier(char specifier, va_list args)
 // {
-// 	ft_printf("Test %% int: %i\n", "12");
+// 	int	count;
+
+// 	if (specifier == 'c')
+// 		count = ft_putchar_fd(1, va_arg(args, int));
+// 	// else if (specifier == 's')
+// 	// 	count = ft_putstr_fd
+// 	// else if (specifier == 'd')
+// 	// 	count = ft_putnbr_fd
+// 	// else if (specifier == 'i')
+// 	// 	count = ft_putnbr_fd
+// 	// else if (specifier == 'u')
+// 	// 	count = ft_putnbr_fd
+// 	// else if (specifier == '%')
+// 	// 	count = ft_putchar_fd
+// 	// else if (specifier == 'x')
+// 	// 	count = ft_puthex
+// 	// else if (specifier == 'X')
+// 	// 	count = ft_puthex_upper
+// 	// else if (specifier == 'p')
+// 	// 	count = ft_putchar_fd or putptr
+// 	else
+// 		return (-1);
+// 	return (count);
 // }
+
+// int	ft_printf(const char *format, ...)
+// {
+// 	int		i;
+// 	va_list	args;
+
+// 	va_start(args, format);
+// 	i = 0;
+// 	while (format[i] != '\0')
+// 	{
+// 		if (format[i] == '%')
+// 			replace_percent(++format, args);
+// 		else
+// 			write(1, &format, 1);
+// 		format++;
+// 	}
+// }
+
+int	main(void)
+{
+	printf("Test int: %s\n", "12");
+	return (0);
+}
