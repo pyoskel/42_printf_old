3fdbd242023594afb0bbd2306db734a17615b1d1
pyoskel <pasigermany@gmail.com>
2025-01-30 02:01:57 +0100

    added the ft_puthex.c for lower and uppercase

--- ft_printf_add_on.c
+++ ft_printf_add_on.c
@@ -6,25 +6,25 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:40:27 by pbartoch          #+#    #+#             */
-/*   Updated: 2025/01/29 22:14:13 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/01/30 01:21:48 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_puthex(unsigned int n, char c)
+int	ft_puthex(unsigned int nbr, char c)
 {
 	int		count;
-	char	digit;
+	char	hexadec;
 
 	count = 0;
-	if (n >= 16)
-		count += ft_puthex(n / 16, c);
+	if (nbr >= 16)
+		count += ft_puthex(nbr / 16, c);
 	if (c == 'X')
-		digit = "0123456789ABCDEF"[n % 16];
+		hexadec = "0123456789ABCDEF"[nbr % 16];
 	else
-		digit = "0123456789abcdef"[n % 16];
-	count += write(1, &digit, 1);
+		hexadec = "0123456789abcdef"[nbr % 16];
+	count += write(1, &hexadec, 1);
 	return (count);
 }
 
