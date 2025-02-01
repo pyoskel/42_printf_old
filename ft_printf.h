223bc87b737a6da9c6f6ad8978e19590ff1649f6
pyoskel <pasigermany@gmail.com>
2025-02-01 23:00:40 +0100

    optimize headerfile

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 01:55:25 by pbartoch          #+#    #+#             */
-/*   Updated: 2025/01/30 01:17:17 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/02/01 23:00:04 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 
 # include <stdarg.h> //for variadic functions
 # include <unistd.h> //for write function
-# include <stdio.h> //for printf function
+// # include <stdio.h>  //for printf function
 
 int	ft_printf(const char *format, ...);
-int	ft_puthex(unsigned int n, char c);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr_base(unsigned int nbr, char c, unsigned int base);
 
 #endif
