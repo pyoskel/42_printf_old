465fc7d15e29f4b4c98b3f74de0a856c73054ff5
pyoskel <pasigermany@gmail.com>
2025-02-02 23:58:56 +0100

    modified headerfile

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 01:55:25 by pbartoch          #+#    #+#             */
-/*   Updated: 2025/02/01 23:00:04 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/02/02 23:23:52 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 # include <stdarg.h> //for variadic functions
 # include <unistd.h> //for write function
-// # include <stdio.h>  //for printf function
+# include <stdio.h>  //for printf function
 
 int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
-int	ft_putnbr_base(unsigned int nbr, char c, unsigned int base);
+int	ft_putnbr_base(int nbr, char c, unsigned int base);
+int	ft_putnbr_u_base(unsigned int nbr, char c, unsigned int base);
+int	ft_putptr(void *ptr);
 
 #endif
